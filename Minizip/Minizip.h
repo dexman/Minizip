798c4c2decb5366bb2e92fca8ec87d844c0865fc
Arthur Dexter <adexter@dexman.net>
2016-02-06 21:41:15 -0600

    Initial empty template project.

---
+++ Minizip/Minizip.h
@@ -0,0 +1,19 @@
+//
+//  Minizip.h
+//  Minizip
+//
+//  Created by Arthur Dexter on 2/6/16.
+//  Copyright © 2016 Arthur Dexter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Minizip.
+FOUNDATION_EXPORT double MinizipVersionNumber;
+
+//! Project version string for Minizip.
+FOUNDATION_EXPORT const unsigned char MinizipVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Minizip/PublicHeader.h>
+
+
